7e350048b95b30dafa2818920e861182da020d74
Nicholas Angelo <40876154+Nicholasangelo@users.noreply.github.com>
2018-08-18 23:35:21 -0400

    game wont end, letters wont appear, I dont know

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,65 +1,95 @@
-// create a list of words
+// MAKE A LIST OF POSSIBLE BIRDS
 var possibleBirds = [
-    "hummingbird",
-    "kingfisher",
-    "albatross",
-    "woodpecker",
-    "nightjar",
-
-];
-guessThisBird = [];
-incorrectLetters = [];
+        "hummingbird",
+        "kingfisher",
+        "albatross",
+        "woodpecker",
+        "nightjar",
+    ];
+// var string;
+var birdIndex = [];
+var randomBird;
+var userGuess;
+var incorrectLetters = [];
+var birdDashes = [];
 
 function newGame() {
-    // pick a random word
-    var randomBird = possibleBirds[Math.floor(Math.random() * possibleBirds.length)];
-
-    // display dashes in place of letters
-    var birdDashes = [];
+// HOW MANY VAR DOES IT TAKE?
+    var guessesRemaining = document.getElementById("remainingGuesses");
+    var incorrect = document.getElementById("incorrect");
+    guessesRemaining.innerHTML = 6;
+    incorrect.innerHTML = [];
+    incorrectLetters = [];
+    birdDashes = [];
+    birdIndex = [];
+    // RANDOMBIRD
+    randomBird = Math.floor(Math.random() * (possibleBirds.length));
+    randomBird = (possibleBirds[randomBird]);
+    // BIRDDASHES
     for (var i = 0; i < randomBird.length; i++) {
-        birdDashes[i] = "_ ";
-
-        // display birdDashes[i] to html element #guessThis
-        var guessThisBird = document.getElementById("guessThis").textContent = (birdDashes);
-
-        // play the game
-        // var userGuess = document.onkeyup = function (event) {
-            // if (gameOver) {
-            //     alert("Game Over! Thanks for killing my bird!");
-            // } else {
-            //     if (event.keycode >= 65 && event.keycode <= 90) {
-            //         userGues(event.key.toLowerCase());
-            //         for (var i = 0; i < birdDashes.length; i++) {
-            //             if (userGuess === birdDashes[i] {
-            //                 alert("Correct!");
+        birdDashes.push(" _ ");
     }
+    //SHOW DASHES
+    var showBirdDashes = document.getElementById("guessThis");
+    showBirdDashes.innerHTML = birdDashes;
+};
+    //MKAES KEYCODE A STRING... MAKES CODE INTO LETTERS
+function string() {
+    userGuess = String.fromCharCode(userGuess);
+};
+//IF WINNER, SOMETHING SHOULD HAPPEN
+function birdsIsTheWord() {
+    if(birdDashes.indexOf(" _ ") === -1) {
+        alert("Congratulations! This bird has flown the coup!")
     }
-    //                 
-    // .push("userGuess")
+};
+//I WANT THIS TO END THE GAME... WHY WONT IT END THE GAME??
+function birdHanged() {
+    document.getElementById("section").style.backgroundImage="url(../images/6.jpg)";
+    alert("What the flock? You killed my bird!");
+};
+//NOT GONNA LIE, STOLEN FROM SLACK CONVO
+function guess() {
+    if (randomBird.indexOf(userGuess) > -1) {
+        for (var i = 0; i < randomBird.length; i++) {
+            if (randomBird[i] === userGuess) {
+                birdIndex.push(i);
+                for (var j = 0; j < birdIndex.length; j++) {
+                    birdDashes[birdIndex[j]] = userGuess;
+                    // birdIndex = [];
+                }
+            }
+        }
+    }
+// INCORRECT GUESSES
+    else {
+        (incorrectLetters.push(userGuess))
+        var guessesRemaining = document.getElementById("remainingGuesses");
+        guessesRemaining.innerHTML--;
+        // getElementById(body).style.backgroundImage=("url(../images/6.jpg)");
+        if (guessesRemaining === 0) {
+        // if (guessesRemaining.innerHTML === 0){
+            birdHanged();
+        }    
+    }
 
-    //             }
-    //         }
-    //     }
-    // }
-    // document.onkeyup = function (event) {
-    //     if (gameOver) {
-    //         resetGame();
-    //         finished = false;
-    //     } else {
-    //         if (event.keyCode >= 65 && event.keyCode <= 90) {
-    //             userGuess(event.key.toLowerCase());
-    //         }
-    //     }
-    // };
 
+    var incorrect = document.getElementById("incorrect");
+    incorrect.innerHTML = "Incorrect Letters: " + incorrectLetters;
+    var birdLetters = document.getElementById("guessThis");
+    birdLetters.innerHTML = birdDashes;
+    birdIsTheWord();
+    
+};
+// I HAD THIS UP HIGHER BUT MAYBE ITS BETTER HERE?
+// function newGame(); {
+document.onkeyup = function (event) {
+    if (event.keyCode >= 65 && event.keyCode <= 90) {
+        (userGuess = event.keyCode);
+        string(userGuess);
+        // guess();
+        guess(userGuess);
+        }
+};
 
-
-
-    // // listen for key
-
-    // // if letter is correct update dash array
-
-    // // if letter is false decremnt score by one and update image
-
-    // // log incorrect guess to incorrect guess pool
 
